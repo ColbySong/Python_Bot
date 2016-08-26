6c7c99578d798acb31801d632ee330f2bcfafc75
ColbySong <colbysong@yahoo.ca>
2016-08-26 11:38:48 -0700

    phone menu button coords

--- code.py
+++ code.py
@@ -28,6 +28,21 @@ class Cord:
     f_salmon = (90, 1110)
     f_unagi = (229, 1116)
 
+    phone = (1433, 957)
+    menu_toppings = (1361, 678)
+
+    t_shrimp = (1238, 570)
+    t_nori = (1237, 707)
+    t_roe = (1466, 706)
+    t_salmon = (1252, 854)
+    t_unagi = (1448, 579)
+    t_exit = (1462, 842)
+
+    menu_rice = (1388, 737)
+    buy_rice = (1379, 734)
+
+    delivary_norm = (1243, 743)
+
 def leftClick():
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.1)
